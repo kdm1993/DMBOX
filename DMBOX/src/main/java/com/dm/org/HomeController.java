cf839f09bdfced25486cfd87a1a95f39e6c57306
kim <kim@211.224.201.88>
2019-12-14 23:10:39 +0900

    0.08
    - Service.jsp에서 검색결과가 있을경우에만 검색결과들을 출력하고 없을경우엔 검색결과가 없다는 텍스트를 출력하는 조건문
    추가
    
    - 검색결과 총 개수를 request 속성에 담아 Service.jsp에서 검색결과 개수를 출력하도록 구현
    
    - 사용자가 검색한 텍스트를 request 속성에 담아 Service.jsp에서 검색결과가 없을경우 출력하도록 구현

--- DMBOX/src/main/java/com/dm/org/HomeController.java
+++ DMBOX/src/main/java/com/dm/org/HomeController.java
@@ -260,73 +260,83 @@ public class HomeController {
 		String clientId = "S6G1FEe39f_eEOMNA58_";// 애플리케이션 클라이언트 아이디값";
 		String clientSecret = "tfFbyN5NS0";// 애플리케이션 클라이언트 시크릿값";\
 		String search_text = request.getParameter("search_text");
+		String total = "0";
+		
+		if(search_text == null) {
+			search_text = "";
+		}
 		ArrayList<MovieDTO> ms = new ArrayList<MovieDTO>();
 		int display = 15; // 검색결과갯수. 최대100개
+		
 		try {
-			String text = URLEncoder.encode(search_text, "utf-8");
-			String apiURL = "https://openapi.naver.com/v1/search/movie.json?query=" + text + "&display=" + display
-					+ "&";
+			if(!search_text.equals("")) {
+				String text = URLEncoder.encode(search_text, "utf-8");
+				String apiURL = "https://openapi.naver.com/v1/search/movie.json?query=" + text + "&display=" + display
+						+ "&";
 
-			URL url = new URL(apiURL);
-			HttpURLConnection con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
-			con.setRequestProperty("X-Naver-Client-Id", clientId);
-			con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-			int responseCode = con.getResponseCode();
-			BufferedReader br;
-			if (responseCode == 200) {
-				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-			} else {
-				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-			}
-			sb = new StringBuilder();
-			String line;
+				URL url = new URL(apiURL);
+				HttpURLConnection con = (HttpURLConnection) url.openConnection();
+				con.setRequestMethod("GET");
+				con.setRequestProperty("X-Naver-Client-Id", clientId);
+				con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+				int responseCode = con.getResponseCode();
+				BufferedReader br;
+				if (responseCode == 200) {
+					br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				} else {
+					br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+				}
+				sb = new StringBuilder();
+				String line;
 
-			while ((line = br.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-			br.close();
-			con.disconnect();
-			JSONParser parser = new JSONParser();
-			Object obj = parser.parse(sb.toString());
-			JSONObject jsonObj = (JSONObject) obj;
-			JSONArray jsonArr = (JSONArray) jsonObj.get("items");
-			
-			for(int x=0; x<jsonArr.size(); x++) {
-				JSONObject jsonObj2 = (JSONObject) jsonArr.get(x);
-				
-				String title = (String) jsonObj2.get("title");
-				String director = (String) jsonObj2.get("director");
-				String sub_title = (String) jsonObj2.get("subtitle");
-				String img = (String) jsonObj2.get("image");
-				String date = (String) jsonObj2.get("pubDate");
-				String rating = (String) jsonObj2.get("userRating");
-				
-				title = title.replace("<b>", "");
-				title = title.replace("</b>", "");
-				img = img.replace("'\'", "");
-				if(img.isEmpty() == true) {
-					img = request.getContextPath()+"/resources/images/poster_default.gif";
+				while ((line = br.readLine()) != null) {
+					sb.append(line + "\n");
 				}
-				if(director.isEmpty() != true) {
-					director = director.substring(0, director.length()-1);					
+				br.close();
+				con.disconnect();
+				JSONParser parser = new JSONParser();
+				Object obj = parser.parse(sb.toString());
+				JSONObject jsonObj = (JSONObject) obj;
+				JSONArray jsonArr = (JSONArray) jsonObj.get("items");
+				total = String.valueOf(jsonObj.get("total"));
+				
+				for(int x=0; x<jsonArr.size(); x++) {
+					JSONObject jsonObj2 = (JSONObject) jsonArr.get(x);
+					
+					String title = (String) jsonObj2.get("title");
+					String director = (String) jsonObj2.get("director");
+					String sub_title = (String) jsonObj2.get("subtitle");
+					String img = (String) jsonObj2.get("image");
+					String date = (String) jsonObj2.get("pubDate");
+					String rating = (String) jsonObj2.get("userRating");
+					
+					title = title.replace("<b>", "");
+					title = title.replace("</b>", "");
+					img = img.replace("'\'", "");
+					if(img.isEmpty() == true) {
+						img = request.getContextPath()+"/resources/images/poster_default.gif";
+					}
+					if(director.isEmpty() != true) {
+						director = director.substring(0, director.length()-1);					
+					}
+					
+					MovieDTO mvdto = new MovieDTO();
+					
+					mvdto.setTitle(title);
+					mvdto.setSubtitle(sub_title);
+					mvdto.setDirector(director);
+					mvdto.setImg(img);
+					mvdto.setDate(date);
+					mvdto.setRating(rating);
+					ms.add(mvdto);
 				}
-				
-				MovieDTO mvdto = new MovieDTO();
-				
-				mvdto.setTitle(title);
-				mvdto.setSubtitle(sub_title);
-				mvdto.setDirector(director);
-				mvdto.setImg(img);
-				mvdto.setDate(date);
-				mvdto.setRating(rating);
-				ms.add(mvdto);
-			}
-			
-			request.setAttribute("searchlist", ms);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			}	
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+		request.setAttribute("searchlist", ms);
+		request.setAttribute("total", total);
+		request.setAttribute("search_text", search_text);
 
 		return "Service";
 	}
--- DMBOX/src/main/webapp/WEB-INF/views/Service.jsp
+++ DMBOX/src/main/webapp/WEB-INF/views/Service.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,27 +10,39 @@
 <body>
 	<jsp:include page="Banner.jsp"></jsp:include>
 	<div id="service" class="container">
-		<c:forEach items="${searchlist}" var="list" varStatus="status" begin="0">    
-			<div class="movielist">
-				<ul>
-					<li>
-						<img src="${list.img}">
-					</li>
-					<li>
-						<a class="title" href="#">${list.title} (${list.subtitle})</a>  
-					</li>
-					<li>
-						<span>${list.date}년 개봉</span>
-					</li>
-					<li>
-						<span>감독 : ${list.director}</span>
-					</li>
-					<li>
-						<span>평점 : ${list.rating}</span>
-					</li>
-				</ul>
-			</div>		
-		</c:forEach>
+		<div>
+			<span>검색결과 ${total}개</span>
+		</div>
+		<c:choose>
+			<c:when test="${not empty searchlist}">
+				<c:forEach items="${searchlist}" var="list" varStatus="status" begin="0">    
+					<div class="movielist">
+						<ul>
+							<li>
+								<img src="${list.img}">
+							</li>
+							<li>   
+								<a class="title" href="#">${list.title} (${list.subtitle})</a>  
+							</li>
+							<li>
+								<span>${list.date}년 개봉</span>
+							</li>
+							<li>
+								<span>감독 : ${list.director}</span>
+							</li>
+							<li>
+								<span>평점 : ${list.rating}</span>
+							</li>
+						</ul>
+					</div>
+				</c:forEach>				
+			</c:when>
+			<c:otherwise>
+				<div style="text-align: center;">
+					<span style="font-weight: bold;">"${search_text}"</span><span>&nbsp;&nbsp;에 대한 검색결과가 없습니다</span>				
+				</div>
+			</c:otherwise>
+		</c:choose>
 	</div>
 	<jsp:include page="Copyright.jsp"></jsp:include>
 </body>
